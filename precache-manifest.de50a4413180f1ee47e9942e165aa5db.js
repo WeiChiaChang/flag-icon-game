f36db762f4c6f307b49bcff20acf7e72c5fff4c7
WeiChiaChang <ioh04019@gmail.com>
2020-02-12 16:00:56 +0800

    Initial dist subtree commit

---
+++ precache-manifest.de50a4413180f1ee47e9942e165aa5db.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "75b215deb896652accc4",
+    "url": "css/app.31746127.css"
+  },
+  {
+    "revision": "75b215deb896652accc4",
+    "url": "js/app.53405965.js"
+  },
+  {
+    "revision": "a893d67c05760191e043",
+    "url": "css/chunk-vendors.04112af6.css"
+  },
+  {
+    "revision": "a893d67c05760191e043",
+    "url": "js/chunk-vendors.0951c290.js"
+  },
+  {
+    "revision": "fb5c6e37a43eab56587f",
+    "url": "css/game.5abe1ef9.css"
+  },
+  {
+    "revision": "fb5c6e37a43eab56587f",
+    "url": "js/game.1255ec8c.js"
+  },
+  {
+    "revision": "9a57c279405618acaf99",
+    "url": "css/game~practice.e397c0a4.css"
+  },
+  {
+    "revision": "9a57c279405618acaf99",
+    "url": "js/game~practice.46a90343.js"
+  },
+  {
+    "revision": "715cec4dbb0d8696a6a4",
+    "url": "css/practice.f82ce99b.css"
+  },
+  {
+    "revision": "715cec4dbb0d8696a6a4",
+    "url": "js/practice.fb949228.js"
+  },
+  {
+    "revision": "de3002f65fb439dd075fe5852d6d555b",
+    "url": "img/kumamon.de3002f6.png"
+  },
+  {
+    "revision": "61c76c88e3e624d63c8dbaa19b63eed3",
+    "url": "fonts/twicon.61c76c88.woff"
+  },
+  {
+    "revision": "a657075b7392dbaf998c6c13121777bb",
+    "url": "fonts/twicon.a657075b.woff2"
+  },
+  {
+    "revision": "54f8c1975a4dc5b23355a1de4a69dc69",
+    "url": "img/the Earth.54f8c197.png"
+  },
+  {
+    "revision": "d4eeb4356cb489e81e46934543cbbce0",
+    "url": "index.html"
+  },
+  {
+    "revision": "47567df77cb0c34606bf4c855cba68cb",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "9f5f8e4cd36ac13901ae15dde763ae9c",
+    "url": "site.webmanifest"
+  },
+  {
+    "revision": "df224f54e21e6d07e8b6db8549042a38",
+    "url": "twicon.css"
+  }
+];
